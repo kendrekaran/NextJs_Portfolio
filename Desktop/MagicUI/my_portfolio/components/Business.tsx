66538f84dd179095dfef2ef8940f1d4c65d13d2d
Karan kendre <142140048+kendrekaran@users.noreply.github.com>
2024-12-09 13:26:34 +0530

    Update Business.tsx

--- Desktop/MagicUI/my_portfolio/components/Business.tsx
+++ Desktop/MagicUI/my_portfolio/components/Business.tsx
@@ -7,7 +7,7 @@ const Business = () => {
   return (
     <div 
     id='freelancing'
-    className='md:py-44 lg:h-screen h-full flex flex-col items-center justify-center xl:px-32'>
+    className=' lg:m-h-screen py-20  md:py-4  h-full flex flex-col items-center justify-center xl:px-32'>
         <motion.h2
           className="text-4xl px-10 sm:py-16 lg:text-7xl font-bold  text-center bg-gradient-to-r from-[#f1f5f966] via-[#f1f5f9] via-51% to-[#f1f5f966] text-transparent bg-clip-text"
           initial={{ opacity: 0 }}
