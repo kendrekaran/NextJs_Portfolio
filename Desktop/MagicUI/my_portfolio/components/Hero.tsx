48c291b1a4414bb189b68d8555723f631724509a
Karan Kendre <142140048+kendrekaran@users.noreply.github.com>
2025-05-30 05:55:37 +0530

    Update Hero.tsx

--- Desktop/MagicUI/my_portfolio/components/Hero.tsx
+++ Desktop/MagicUI/my_portfolio/components/Hero.tsx
@@ -20,7 +20,7 @@ const Hero = () => {
       <div className="flex flex-col-reverse md:flex-row justify-center md:gap-10 sm:py-4 items-center mt-8 md:mt-0">
         <div className="text-center md:text-left md:px-4 md:w-1/2">
           <div className="text-sm px-2 sm:px-0 font-normal text-white dark:text-white"
-          >I&apos;m a 21-year-old BCS student and aspiring full-stack developer. I&apos;m skilled in Next.js, and the MERN stack, with a passion for building creative and efficient applications. Currently, I&apos;m focused on honing my development skills and exploring new skills.
+          >I&apos;m a 21-year-old BCS student and frontend developer. I&apos;m skilled in Next.js, and the MERN stack, with a passion for building creative and efficient applications. Currently, I&apos;m focused on honing my development skills and exploring new skills.
           </div>
         </div>
         <div className="mb-6 md:mb-0">
